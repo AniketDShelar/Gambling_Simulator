23045da4cf91ae32bd2417d5dec53a5312f837f4
Aniket Shelar <theaniketshelar@gmail.com>
2022-07-21 19:14:00 +0530

    Refactored : UC 2, as a make a bet to win or lose

--- src/com/bridgelabz/gamblingsimulator/Gambling.java
+++ src/com/bridgelabz/gamblingsimulator/Gambling.java
@@ -2,8 +2,19 @@ package com.bridgelabz.gamblingsimulator;
 
 public class Gambling {
 	int stake = 100;
-	final int dailybet = 1;
+	static int dailybet = 1;
+	public static void gambler() {
+		int result = (int) Math.floor(Math.random() * 10 % 2);
+		if(dailybet == result) {
+			System.out.println("Winner");
+		}else {
+			System.out.println("loser");
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulator Program");
+		gambler();
 	}
+
 }
