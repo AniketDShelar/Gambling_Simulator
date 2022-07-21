286696956179b24a5d5025f9cfb0319a16ca8f2f
Aniket Shelar <theaniketshelar@gmail.com>
2022-07-21 19:11:19 +0530

    Added : UC 1, Started stake with 00 and daily bet with

---
+++ src/com/bridgelabz/gamblingsimulator/Gambling.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.gamblingsimulator;
+
+public class Gambling {
+	int stake = 100;
+	final int dailybet = 1;
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulator Program");
+	}
+}
